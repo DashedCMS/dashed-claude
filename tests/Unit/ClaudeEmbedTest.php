4f140f5c76dab674874e6c926d129830d0a6832f
Robin van Maasakker <robin@dashed.nl>
2026-04-13 14:52:07 +0200

    style(claude): apply php-cs-fixer formatting

--- tests/Unit/ClaudeEmbedTest.php
+++ tests/Unit/ClaudeEmbedTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use Dashed\DashedAi\Exceptions\EmbeddingNotSupportedException;
-use Dashed\DashedClaude\ClaudeProvider;
 use Tests\TestCase;
+use Dashed\DashedClaude\ClaudeProvider;
+use Dashed\DashedAi\Exceptions\EmbeddingNotSupportedException;
 
 uses(TestCase::class);
 
 it('throws EmbeddingNotSupportedException when embed() is called', function () {
-    $provider = new ClaudeProvider;
+    $provider = new ClaudeProvider();
 
     expect(fn () => $provider->embed('some text'))
         ->toThrow(EmbeddingNotSupportedException::class);
